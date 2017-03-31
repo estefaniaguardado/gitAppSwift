9a830278e574a463f0b145befaf55af8680bd409
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-03-31 14:26:33 +0200

    Init project iOS

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Estefania Guardado on 31.03.17.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
