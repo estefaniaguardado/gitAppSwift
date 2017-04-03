bd509c2c351edcf8ac04ea6d9648e4ebb73a6e9b
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-04-03 10:00:01 +0200

    Init CollectionVC with custom Cells

---
+++ gitApp/ResultCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ResultCollectionViewCell.swift
+//  gitApp
+//
+//  Created by Estefania Guardado on 03.04.17.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+import UIKit
+
+class ResultCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
