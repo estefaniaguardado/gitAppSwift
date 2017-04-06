794394f2a0d6ff402bc2c762d5eb7d504dd271d4
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-04-06 10:26:29 +0200

    Create Repository Structs

---
+++ gitApp/Repository.swift
@@ -0,0 +1,18 @@
+//
+//  Repositorie.swift
+//  gitApp
+//
+//  Created by Estefania Guardado on 06.04.17.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+import Foundation
+
+struct Repository{
+    
+    var name = String()
+    var language = String()
+    var forksCount = Int()
+    var owner = RepositoryOwner()
+
+}
---
+++ gitApp/RepositoryOwner.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryOwner.swift
+//  gitApp
+//
+//  Created by Estefania Guardado on 06.04.17.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+import Foundation
+
+struct RepositoryOwner {
+    
+    var nameLogin = String()
+    var avatar = URL (string: String())
+    
+}
