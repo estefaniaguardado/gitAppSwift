89e32ac6ead40aefea50b5108305b63e5cc55355
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-04-08 13:59:11 +0200

    Define protocol RepositorDatasource

---
+++ gitApp/Protocols/IRepositoryDatasource.swift
@@ -0,0 +1,15 @@
+//
+//  IRepositoryDataSource.swift
+//  gitApp
+//
+//  Created by Estefania Guardado on 08.04.17.
+//  Copyright © 2017 Larsecg. All rights reserved.
+//
+
+import Foundation
+
+protocol IRepositoryDataSource {
+    
+    func getRepositories() -> Array<Repository>
+
+}
