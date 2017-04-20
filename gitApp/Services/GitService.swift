eb3a50c827d620610eae00c5a1d975e39ef20387
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-04-20 15:46:03 +0200

    Check if exist repository info in previous request

--- gitApp/Services/GitService.swift
+++ gitApp/Services/GitService.swift
@@ -11,6 +11,7 @@ import Foundation
 let GITHUB_REPOSITORIES_URL_TOPIC = "https://api.github.com/search/repositories?q=topic:"
 let GITHUB_REPOSITORIES_URL_ORDER = "&sort=stars&order=desc"
 let GITHUB_REPOSITORIES_URL_PERPAGE = "&per-page=30"
+var setIds = Set<Int>()
 
 class GitService: IRepositoryDataSource {
 
@@ -61,26 +62,34 @@ class GitService: IRepositoryDataSource {
         var repositoryArray = [Repository]()
         
         for (_, item) in items.enumerated(){
-            repositoryArray.append(self.initializeRepositoryData(data: item))
+            let itemID = item.value(forKey: "id") as! Int
+            
+            if !self.existRepository(id: itemID) {
+                repositoryArray.append(self.initializeRepositoryData(id: itemID, data: item))
+            }
         }
         
         return repositoryArray
     }
     
-    func initializeRepositoryData(data: NSDictionary) -> Repository {
+    func existRepository(id: Int) -> Bool {
+        if setIds.contains(id) {
+            return true
+        }
+        
+        setIds.insert(id)
+        return false
+    }
+    
         let itemName = data.value(forKey: "name") as! String
         let itemLanguage = (data.value(forKey: "language") as? String != nil) ?
-                            data.value(forKey: "language") as! String : ""
+                data.value(forKey: "language") as! String : ""
         let itemForks = data.value(forKey: "forks_count") as! Int
         let owner = data.value(forKey: "owner") as! NSDictionary
         let loginOwner = owner.value(forKey: "login") as! String
         let avatarOwner = (owner.value(forKey: "avatar_url") as? String != nil) ?
-                           owner.value(forKey: "avatar_url") as! String : ""
-        
-        return Repository.init(name: itemName,
-                               repoLanguage: itemLanguage,
-                               forks: itemForks,
-                               owner: loginOwner,
+                owner.value(forKey: "avatar_url") as! String : ""
+            
                                imageURL: URL.init(string: avatarOwner)!)
         
     }
