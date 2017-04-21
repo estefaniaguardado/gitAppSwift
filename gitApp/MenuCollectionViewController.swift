33d50cb5dca648069e8551f6fd1a9d26931ceaf3
Estefania Chavez Guardado <fachinacg@gmail.com>
2017-04-21 19:51:52 +0200

    Minor refactoring & TODOs

--- gitApp/MenuCollectionViewController.swift
+++ gitApp/MenuCollectionViewController.swift
@@ -14,23 +14,24 @@ private let reuseIdentifier = "Cell"
 private let blueDarkColor = UIColor.init(red: 0.101, green: 0.321, blue: 0.462, alpha: 0) //26.82.118
 
 class MenuCollectionViewController: UICollectionViewController, UITextFieldDelegate, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
-    
+
     private let gitService = GitService()
     private var repositoriesData = [Repository]()
     private var downloadedImages = [UIImage]()
-    
+
     private var pageNumber = 1
     private var isLoading = false
     private var searchActive = false
     private var searchTerm = String()
     private var resultsCount = Int()
-    
+    private var setTest = Set<Repository>()
+
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet var searchButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         searchTextField.delegate = self
         collectionView?.emptyDataSetSource = self
         collectionView?.emptyDataSetDelegate = self
@@ -42,9 +43,9 @@ class MenuCollectionViewController: UICollectionViewController, UITextFieldDeleg
         self.searchActive = false
         self.searchTextField.resignFirstResponder()
         let query = searchTextField.text?.trimmingCharacters(in: .whitespaces)
-        if (query?.isEmpty)!{
+        if (query?.isEmpty)! {
             self.presentAlertWhenAccessToData(title: "Empty search",
-                                         message: "Complete with search term for the researching")
+                    message: "Complete with search term for the researching")
         } else {
             searchTerm = (query?.replacingOccurrences(of: " ", with: "-"))!
             searchTextField.text = searchTerm
@@ -52,68 +53,85 @@ class MenuCollectionViewController: UICollectionViewController, UITextFieldDeleg
             getGitData()
         }
     }
-    
-    func presentAlertWhenAccessToData(title:String, message: String){
+
+    func presentAlertWhenAccessToData(title: String, message: String) {
         let alert = UIAlertController.init(title: title,
-                                           message: message,
-                                           preferredStyle: .alert)
-        
+                message: message,
+                preferredStyle: .alert)
+
         let defaultAction = UIAlertAction.init(title: "Ok",
-                                               style: .default)
-        
+                style: .default)
+
         alert.addAction(defaultAction)
         self.present(alert, animated: true, completion: nil)
     }
-    
-    func customizationOutlets(isEnable:Bool, color:UIColor) {
+
+    func customizationOutlets(isEnable: Bool, color: UIColor) {
         self.searchTextField.isUserInteractionEnabled = isEnable
         self.searchTextField.textColor = color
         self.searchButton.isEnabled = isEnable
         self.searchButton.tintColor = color
     }
 
-    func getGitData () -> Void {
+    func getGitData() -> Void {
         let progressHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
         progressHUD.label.text = "Searching"
         progressHUD.mode = .indeterminate
 
         gitService.getRepositories(searchTerm: searchTerm, pageNumber: String(pageNumber)) {
             results, error in
-            
+
+
+            // DISPATCH
+
             let loadingCollection = DispatchGroup()
             loadingCollection.enter()
+
+            // TODO: Extract methods to simplify the view logic
+
+            /*let isEmpty = XXX
+            let continueScrolling = XXXX
             
+            if (error) return presentError(error);
+
+            if(isEmpty) return presentEmptyAlert();
+            if(continueScrolling) return XXX();*/
+
+
+            let isEmpty = (results?.isEmpty)! && !self.searchActive
+            let noSeEnSerio = (results?.isEmpty)! && self.searchActive
             if let error = error {
                 print("Error searching : \(error)")
                 return
-                
-            } else if ((results?.isEmpty)! && !self.searchActive) {
+
+            } else if (isEmpty) {
                 loadingCollection.leave()
-                loadingCollection.notify(queue: .main){
+                loadingCollection.notify(queue: .main) {
                     progressHUD.hide(animated: true)
                     self.customizationOutlets(isEnable: true, color: .white)
                     self.presentAlertWhenAccessToData(title: "Don't found results", message: "")
                 }
                 return
-                
-            } else if ((results?.isEmpty)! && self.searchActive) {
+
+            } else if (noSeEnSerio) {
                 loadingCollection.leave()
-                loadingCollection.notify(queue: .main){
+                loadingCollection.notify(queue: .main) {
                     self.searchActive = false
                     progressHUD.hide(animated: true)
                     self.customizationOutlets(isEnable: true, color: .white)
                 }
                 return
-                
+
             } else {
-                
-                var lastIndexResults = Int()
-                
+
+                var lastIndexResults: Int
+
                 if (self.isLoading) {
                     self.repositoriesData += results!
                     lastIndexResults = self.resultsCount
                     self.resultsCount += (results?.count)!
                 } else {
+                    // TODO: Move repositories cached to the Search Action
                     self.repositoriesData.removeAll()
                     self.repositoriesData = results!
                     self.downloadedImages.removeAll()
@@ -121,37 +139,44 @@ class MenuCollectionViewController: UICollectionViewController, UITextFieldDeleg
                     lastIndexResults = self.resultsCount
                     self.resultsCount = (results?.count)!
                 }
-                
-                DispatchQueue.main.async{
-                    
+
+                DispatchQueue.main.async {
+
                     var arrayIndexPath = [IndexPath]()
 
-                    for index in lastIndexResults...self.resultsCount - 1{
+                    for index in lastIndexResults...self.resultsCount - 1 {
                         let repository = self.repositoriesData[index]
                         self.downloadImageFromURL(imageURL: repository.ownerAvatar)
                         arrayIndexPath.append((IndexPath.init(row: index, section: 0)))
                     }
-                    
-                    if (self.isLoading){
+
+                    print("Indexpaths: \(arrayIndexPath)")
+
+                    // Use variable based on view model and not based on view flag
+                    if (self.isLoading) {
                         self.collectionView?.insertItems(at: arrayIndexPath)
                         self.isLoading = false
-                    } else{
+                    } else {
                         self.collectionView?.reloadData()
                     }
 
                     loadingCollection.leave()
                 }
-                
-                loadingCollection.notify(queue: .main){
+
+                loadingCollection.notify(queue: .main) {
                     self.customizationOutlets(isEnable: true, color: .white)
                     progressHUD.hide(animated: true)
                     self.searchActive = true
                 }
             }
         }
-        
+
     }
-    
+
+    func presentAlert() {
+
+    }
+
     func downloadImageFromURL(imageURL: URL) {
         let imageData = NSData(contentsOf: imageURL)!
         downloadedImages.append(UIImage(data: imageData as Data)!)
@@ -162,30 +187,30 @@ class MenuCollectionViewController: UICollectionViewController, UITextFieldDeleg
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+
         let viewCellData = repositoriesData[indexPath.row]
         let imageOwnerRepository = downloadedImages[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! RepositoryCollectionViewCell
         cell.setData(repositoryData: viewCellData, imageOwner: imageOwnerRepository)
-        
+
         return cell
     }
-    
+
     func image(forEmptyDataSet scrollView: UIScrollView!) -> UIImage! {
         return UIImage.init(named: "repository")
     }
-    
+
     func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
         let titleText = "Not repositories to show"
-        
+
         let attributes = [
-            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 16),
-            NSForegroundColorAttributeName: UIColor.lightGray
+                NSFontAttributeName: UIFont.boldSystemFont(ofSize: 16),
+                NSForegroundColorAttributeName: UIColor.lightGray
         ]
-        
+
         return NSAttributedString.init(string: titleText, attributes: attributes)
     }
-    
+
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offset = scrollView.contentOffset.y
         let maxOffset = scrollView.contentSize.height - scrollView.frame.size.height
@@ -197,5 +222,5 @@ class MenuCollectionViewController: UICollectionViewController, UITextFieldDeleg
             }
         }
     }
-    
+
 }
